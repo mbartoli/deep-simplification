679c97325e797fb2ca2402c0ba60610b6a875030
mbartoli <bartolimichael@gmail.com>
2015-07-30 05:27:35 -0400

    refactor, shell script, processed data

---
+++ data/tokenize.sh
@@ -0,0 +1,6 @@
+perl ./tokenizer.perl -l en < ./training/training-normal > ./training/training-normal.tok
+perl ./tokenizer.perl -l en < ./training/training-simple > ./training/training-simple.tok
+perl ./tokenizer.perl -l en < ./validation/validation-normal > ./training/validation-normal.tok
+perl ./tokenizer.perl -l en < ./validation/validation-simple > ./training/validation-simple.tok
+
+
