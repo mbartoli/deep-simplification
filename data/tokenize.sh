3edb12e6141288aff23178dec7c6c3a36f7793bf
mbartoli <bartolimichael@gmail.com>
2015-07-30 05:31:07 -0400

    moving validation data

--- data/tokenize.sh
+++ data/tokenize.sh
@@ -1,6 +1,6 @@
 perl ./tokenizer.perl -l en < ./training/training-normal > ./training/training-normal.tok
 perl ./tokenizer.perl -l en < ./training/training-simple > ./training/training-simple.tok
-perl ./tokenizer.perl -l en < ./validation/validation-normal > ./training/validation-normal.tok
-perl ./tokenizer.perl -l en < ./validation/validation-simple > ./training/validation-simple.tok
+perl ./tokenizer.perl -l en < ./validation/validation-normal > ./validation/validation-normal.tok
+perl ./tokenizer.perl -l en < ./validation/validation-simple > ./validation/validation-simple.tok
 
 
