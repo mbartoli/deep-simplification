6ba22908e5b168c59a46b47c00191674c7e4c929
mbartoli <bartolimichael@gmail.com>
2016-02-19 02:02:19 -0800

    adding tokenized dictionaires for 212k dataset

--- data/split_raw.py
+++
@@ -1,7 +0,0 @@
-import csv 
-import sys
-
-infile = sys.argv[1]
-outfile = sys.argv[2]
-
-with open
--- data/tokenize.sh
+++ data/tokenize.sh
@@ -1,4 +1,6 @@
-perl ./tokenizer.perl -l en < ./training/training-normal > ./training/training-normal.tok
-perl ./tokenizer.perl -l en < ./training/training-simple > ./training/training-simple.tok
-perl ./tokenizer.perl -l en < ./validation/validation-normal > ./validation/validation-normal.tok
-perl ./tokenizer.perl -l en < ./validation/validation-simple > ./validation/validation-simple.tok
+perl ./tokenizer.perl -l en < ./training/training.normal.aligned > ./training/training.normal.aligned.tok
+perl ./tokenizer.perl -l en < ./training/training.simple.aligned > ./training/training.simple.aligned.tok
+perl ./tokenizer.perl -l en < ./validation/validation.normal.aligned > ./validation/validation.normal.aligned.tok
+perl ./tokenizer.perl -l en < ./validation/validation.simple.aligned > ./validation/validation.simple.aligned.tok
+perl ./tokenizer.perl -l en < ./testing/testing.normal.aligned > ./testing/testing.normal.aligned.tok
+perl ./tokenizer.perl -l en < ./testing/testing.simple.aligned > ./testing/testing.simple.aligned.tok
